39e0eb37093ab4dfbff1624780a91fcb77e227c2
Christopher Cuadrado <cuadradochris@hotmail.com>
2022-12-19 18:21:04 -0500

    routes + some pages set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import { NavBar } from "./components/layout/NavBar";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
@@ -8,8 +11,15 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <NavBar />
 
-        <main className="container mx-auto px-3 pb-12">Content</main>
-        <Footer/>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+
+
+export const About = () => {
+  return (
+    <div>
+        <h1 className="text-6xl mb-4">Github API</h1>
+        <p className='mb-4 text-2xl font-light'>
+        A React app that consumes Githubs API to search user profiles and view details. Enjoy.
+      </p>
+    </div>
+  )
+}
