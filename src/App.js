55ee196fa7764f358c11c9278d47f94c32d59144
Christopher Cuadrado <cuadradochris@hotmail.com>
2022-12-20 15:34:52 -0500

    user component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { GithubProvider } from "./context/github/GithubContext";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+import { User } from "./pages/User";
 
 function App() {
   return (
@@ -17,10 +18,11 @@ function App() {
             <NavBar />
 
             <main className="container mx-auto px-3 pb-12">
-              <Alert/>
+              <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
---
+++ src/pages/User.jsx
@@ -0,0 +1,14 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import GithubContext from "../context/github/GithubContext";
+
+export const User = () => {
+  const { user, getUser } = useContext(GithubContext);
+  const params = useParams();
+
+  useEffect(() => {
+    getUser(params.login);
+  }, []);
+
+  return <div>{user.login}</div>;
+};
